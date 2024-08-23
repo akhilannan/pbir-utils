e3f3c99764dd9412eb2e362feb774a1cf2430ca1
akhilannan <97329617+akhilannan@users.noreply.github.com>
2024-08-23 14:46:10 +0530

    improvements to workflow

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pbir-utils', 
-    version='0.1.0',
+    version='0.1.2',
     description='A tool for managing Power BI Enhanced Report Format (PBIR) projects',
     long_description=open('README.md').read(), 
     long_description_content_type='text/markdown',  
