da1d9facdf77f05b5b66fe4a52a772065ba7df1a
akhilannan <97329617+akhilannan@users.noreply.github.com>
2024-09-08 20:39:50 +0530

    Initial version of filter_utils

--- src/pbir_utils/__init__.py
+++ src/pbir_utils/__init__.py
@@ -3,6 +3,7 @@ from .metadata_extractor import export_pbir_metadata_to_csv
 from .report_wireframe_visualizer import display_report_wireframes
 from .visual_interactions_utils import disable_visual_interactions
 from .pbir_measure_utils import remove_measures, generate_measure_dependencies_report
+from .filter_utils import update_report_filters, sort_report_filters
 
 __all__ = [
     "batch_update_pbir_project",
@@ -11,4 +12,6 @@ __all__ = [
     "disable_visual_interactions",
     "remove_measures",
     "generate_measure_dependencies_report",
+    "update_report_filters",
+    "sort_report_filters",
 ]
