a83277d3b5654259c4f5884010580528849cc77e
akhilannan <97329617+akhilannan@users.noreply.github.com>
2024-10-16 17:31:30 +0530

    wireframe page sorted

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,2 @@
-## New Functions:
-- *update_report_filters*: Enables bulk updating of report-level filters in the Power BI filter pane.
-- *sort_report_filters*: Allows custom sorting of report-level filter pane attributes.
-
 ## Improvements:
-Added type annotations to all function parameters.
+Report Wireframe page sorting corrected.
--- src/pbir_utils/report_wireframe_visualizer.py
+++ src/pbir_utils/report_wireframe_visualizer.py
@@ -14,7 +14,7 @@ def _extract_page_info(page_folder: str) -> tuple:
         page_folder (str): Path to the page folder containing the `page.json` file.
 
     Returns:
-        tuple: A tuple containing the page display name, width, and height.
+        tuple: A tuple containing the page ID, display name, width, and height.
 
     Raises:
         FileNotFoundError: If the `page.json` file does not exist in the specified folder.
@@ -22,13 +22,16 @@ def _extract_page_info(page_folder: str) -> tuple:
     page_json_path = os.path.join(page_folder, "page.json")
 
     if not os.path.exists(page_json_path):
-        # Raise an error if the `page.json` file is missing
         raise FileNotFoundError(f"{page_json_path} does not exist")
 
     page_data = _load_json(page_json_path)
 
-    # Return the display name, width, and height from the page data
-    return page_data["displayName"], page_data["width"], page_data["height"]
+    return (
+        page_data["name"],
+        page_data["displayName"],
+        page_data["width"],
+        page_data["height"],
+    )
 
 
 def _extract_visual_info(visuals_folder: str) -> dict:
@@ -43,16 +46,14 @@ def _extract_visual_info(visuals_folder: str) -> dict:
               Each tuple contains (x, y, width, height, visualType, parentGroupName, isHidden).
     """
     visuals = {}
-    # Iterate through each subdirectory in the visuals folder
     for visual_id in os.listdir(visuals_folder):
         visual_json_path = os.path.join(visuals_folder, visual_id, "visual.json")
         if not os.path.exists(visual_json_path):
-            continue  # Skip if the visual.json file does not exist
+            continue
 
         visual_data = _load_json(visual_json_path)
         position = visual_data["position"]
 
-        # Extract and store visual information in a tuple
         visuals[visual_id] = (
             position["x"],
             position["y"],
@@ -71,16 +72,13 @@ def _adjust_visual_positions(visuals: dict) -> dict:
     Adjust visual positions based on parent-child relationships.
 
     Args:
-        visuals (dict): Dictionary with visual information. Each key is a visual ID and
-                        the value is a tuple containing (x, y, width, height, name, parent, is_hidden).
+        visuals (dict): Dictionary with visual information.
 
     Returns:
-        dict: Dictionary with adjusted visual positions, where each visual's position is updated
-              according to its parent's position.
+        dict: Dictionary with adjusted visual positions.
     """
     return {
         vid: (
-            # Adjust position based on parent's coordinates, if parent exists
             x + visuals[parent][0] if parent in visuals else x,
             y + visuals[parent][1] if parent in visuals else y,
             width,
@@ -108,24 +106,20 @@ def _create_wireframe_figure(
     Returns:
         go.Figure: Plotly figure object for the wireframe.
     """
-
     fig = go.Figure()
 
-    # Adjust visual positions and sort by name and visual_id
     adjusted_visuals = _adjust_visual_positions(visuals_info)
     sorted_visuals = sorted(adjusted_visuals.items(), key=lambda x: (x[1][4], x[0]))
 
     legend_labels = []
     for visual_id, (x, y, width, height, name, _, is_hidden) in sorted_visuals:
         if not show_hidden and is_hidden:
-            continue  # Skip hidden visuals if not showing them
+            continue
 
         line_style = "dot" if is_hidden else "solid"
-        # Calculate center of the box
         center_x = x + width / 2
         center_y = y + height / 2
 
-        # Add the visual rectangle with an invisible line to the center
         if name != "Group":
             label = f"{name} ({visual_id})"
             legend_labels.append(label)
@@ -144,7 +138,6 @@ def _create_wireframe_figure(
                 )
             )
 
-    # Update layout space to include legend and adjust dimensions
     legend_width_pixel = max(len(label) for label in legend_labels) * 7
     fig.update_layout(
         width=page_width + legend_width_pixel,
@@ -175,15 +168,11 @@ def _apply_filters(
     Returns:
         list: Filtered list of tuples containing page information.
     """
-
     filtered_pages_info = []
-    for page_name, page_width, page_height, visuals_info in pages_info:
-
-        # Skip this page if it's not in the specified pages list
-        if pages and page_name not in pages:
+    for page_id, page_name, page_width, page_height, visuals_info in pages_info:
+        if pages and page_id not in pages:
             continue
 
-        # Filter visuals based on visual_types or visual_ids
         filtered_visuals_info = {
             vid: vinfo
             for vid, vinfo in visuals_info.items()
@@ -191,20 +180,18 @@ def _apply_filters(
             and (not visual_ids or vid in visual_ids)
         }
 
-        # Collect parent visuals to add after the loop
         parents_to_add = {
             parent_id: visuals_info[parent_id]
             for _, vinfo in filtered_visuals_info.items()
             if (parent_id := vinfo[5]) and parent_id not in filtered_visuals_info
         }
 
-        # Add parent visuals to the filtered visuals dictionary
         filtered_visuals_info.update(parents_to_add)
 
-        # Add the page to the result if there are filtered visuals or no visual filters were applied
         if filtered_visuals_info or (not visual_types and not visual_ids):
             filtered_pages_info.append(
                 (
+                    page_id,
                     page_name,
                     page_width,
                     page_height,
@@ -213,6 +200,21 @@ def _apply_filters(
             )
 
     return filtered_pages_info
+
+
+def _get_page_order(report_path: str) -> list:
+    """
+    Get the page order from the pages.json file.
+
+    Args:
+        report_path (str): Path to the root folder of the report.
+
+    Returns:
+        list: List of page IDs in the correct order.
+    """
+    pages_json_path = os.path.join(report_path, "definition", "pages", "pages.json")
+    pages_data = _load_json(pages_json_path)
+    return pages_data["pageOrder"]
 
 
 def display_report_wireframes(
@@ -227,16 +229,14 @@ def display_report_wireframes(
 
     Args:
         report_path (str): Path to the root folder of the report.
-        pages (list, optional): List of page names to include. Defaults to None.
+        pages (list, optional): List of page IDs to include. Defaults to None.
         visual_types (list, optional): List of visual types to include. Defaults to None.
         visual_ids (list, optional): List of visual IDs to include. Defaults to None.
         show_hidden (bool, optional): Flag to determine if hidden visuals should be shown. Defaults to True.
     """
-    # Path to the folder containing page definitions
     pages_folder = os.path.join(report_path, "definition", "pages")
     pages_info = []
 
-    # Collect page and visual information
     for page_folder in filter(
         lambda x: os.path.isdir(os.path.join(pages_folder, x)), os.listdir(pages_folder)
     ):
@@ -254,33 +254,35 @@ def display_report_wireframes(
         print("No pages found.")
         return
 
-    # Apply filters to the collected pages_info
     filtered_pages_info = _apply_filters(pages_info, pages, visual_types, visual_ids)
     if not filtered_pages_info:
         print("No pages match the given filters.")
         return
 
-    # Initialize Dash app and layout
+    page_order = _get_page_order(report_path)
+    sorted_pages_info = sorted(
+        filtered_pages_info, key=lambda x: page_order.index(x[0])
+    )
+
     app = dash.Dash(__name__)
     app.layout = html.Div(
         [
             dcc.Tabs(
                 id="tabs",
-                value=filtered_pages_info[0][0],
+                value=sorted_pages_info[0][0],
                 children=[
-                    dcc.Tab(label=page_name, value=page_name)
-                    for page_name, _, _, _ in filtered_pages_info
+                    dcc.Tab(label=page_name, value=page_id)
+                    for page_id, page_name, _, _, _ in sorted_pages_info
                 ],
             ),
             html.Div(id="tab-content"),
         ]
     )
 
-    # Callback to render content based on selected tab
     @app.callback(Output("tab-content", "children"), Input("tabs", "value"))
     def render_content(selected_tab: str):
-        for _, page_width, page_height, visuals_info in filter(
-            lambda item: item[0] == selected_tab, filtered_pages_info
+        for _, _, page_width, page_height, visuals_info in filter(
+            lambda item: item[0] == selected_tab, sorted_pages_info
         ):
             fig = _create_wireframe_figure(
                 page_width, page_height, visuals_info, show_hidden
@@ -288,5 +290,4 @@ def display_report_wireframes(
             return dcc.Graph(figure=fig)
         return html.Div("Page not found")
 
-    # Run the Dash server
     app.run_server(debug=True)
